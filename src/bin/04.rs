fb4b4a0721e86fcbb7934838a951319294882df5
Dantos7 <daniel.tosone@gmail.com>
2023-12-04 19:22:54 +0100

    Day 4 - Part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,4 +1,6 @@
 use qndr::get_numbers;
+use std::collections::HashMap;
+use tqdm::tqdm;
 
 advent_of_code::solution!(4);
 
@@ -17,9 +19,9 @@ fn parse_card(line: &str) -> (u32, Vec<u32>, Vec<u32>) {
     let mut split_iter = line.split(": ");
     let card_id = parse_int(split_iter.nth(0).unwrap()).unwrap();
     let mut numbers_split_iter = split_iter.nth(0).unwrap().split(" | ");
-    let mut winning_numbers_split = numbers_split_iter.nth(0).unwrap().split_whitespace();
+    let winning_numbers_split = numbers_split_iter.nth(0).unwrap().split_whitespace();
     let winning_numbers: Vec<u32> = winning_numbers_split.map(|s| parse_int(s).unwrap()).collect();
-    let mut own_numbers_split = numbers_split_iter.nth(0).unwrap().split_whitespace();
+    let own_numbers_split = numbers_split_iter.nth(0).unwrap().split_whitespace();
     let own_numbers: Vec<u32> = own_numbers_split.map(|s| parse_int(s).unwrap()).collect();
 
     (card_id, winning_numbers, own_numbers)
@@ -45,7 +47,42 @@ fn parse_int(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut sum = 0;
+    let mut cards_count: HashMap<u32, u32> = HashMap::new();
+    for (row, line) in tqdm(input.split("\n").enumerate()) {
+        if line.len() > 0 {
+            let card_id = row as u32 + 1;
+            match cards_count.get(&card_id) {
+                Some(c) => cards_count.insert(card_id, *c),
+                None => cards_count.insert(card_id, 1),
+            };
+            let card_count = *cards_count.get(&card_id).unwrap();
+            for _copy in 0..card_count {
+                let (card_id, winning_numbers, own_numbers) = parse_card(line);
+                let matches = get_number_of_matches(winning_numbers, own_numbers);
+                for i in (card_id + 1)..(card_id + matches + 1) {
+                    match cards_count.get(&i) {
+                        Some(c) => cards_count.insert(i, c + 1),
+                        None => cards_count.insert(i, 2),
+                    };
+                }
+            }
+        }
+    }
+    for (_, v) in cards_count.iter() {
+        sum += v;
+    }
+    Some(sum)
+}
+
+fn get_number_of_matches(winning_numbers: Vec<u32>, own_numbers: Vec<u32>) -> u32 {
+    let mut matches = 0;
+    for wn in winning_numbers.iter() {
+        if own_numbers.contains(wn) {
+            matches += 1;
+        }
+    }
+    matches
 }
 
 #[cfg(test)]
@@ -61,6 +98,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
