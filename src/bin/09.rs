b4d92b3b4b1533ba40c3f8c4f088cf9a6ebf076b
Dantos7 <daniel.tosone@gmail.com>
2023-12-09 13:09:33 +0100

    Day 9 - Part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -10,7 +10,8 @@ pub fn part_one(input: &str) -> Option<i32> {
         }
     }
     for sequence in sequences {
-        let next_number = get_next_number_of_sequence(&sequence);
+        let differences = get_differences(&sequence);
+        let next_number = get_next_number_forward(differences);
         sum += next_number;
     }
     Some(sum)
@@ -20,7 +21,7 @@ fn parse_line(line: &str) -> Vec<i32> {
     line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>()
 }
 
-fn get_next_number_of_sequence(sequence: &Vec<i32>) -> i32 {
+fn get_differences(sequence: &Vec<i32>) -> Vec<Vec<i32>> {
     let mut differences = vec![];
     let mut difference = (*sequence).clone();
     while !is_all_zeros(&difference) {
@@ -33,7 +34,10 @@ fn get_next_number_of_sequence(sequence: &Vec<i32>) -> i32 {
         }
         difference = new_difference.clone();
     }
+    differences
+}
 
+fn get_next_number_forward(mut differences: Vec<Vec<i32>>) -> i32 {
     // Compute the next value iteratively from the back
     let mut next_number = 0;
     for _ in 0..differences.len() {
@@ -48,7 +52,30 @@ fn is_all_zeros(vec: &Vec<i32>) -> bool {
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
-    None
+    let mut sum = 0;
+    let mut sequences: Vec<Vec<i32>> = vec![];
+    for line in input.split("\n") {
+        if line.len() > 0 {
+            let sequence = parse_line(line);
+            sequences.push(sequence);
+        }
+    }
+    for sequence in sequences {
+        let differences = get_differences(&sequence);
+        let next_number = get_next_number_backwards(differences);
+        sum += next_number;
+    }
+    Some(sum)
+}
+
+fn get_next_number_backwards(mut differences: Vec<Vec<i32>>) -> i32 {
+    // Compute the next value iteratively from the back
+    let mut next_number = 0;
+    for _ in 0..differences.len() {
+        let difference = differences.pop().unwrap();
+        next_number = difference.first().unwrap() - next_number;
+    }
+    next_number
 }
 
 #[cfg(test)]
@@ -64,6 +91,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
