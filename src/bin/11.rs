b8027516b77650e781c15f6cecf7183ae81b16f3
Dantos7 <daniel.tosone@gmail.com>
2023-12-11 17:54:47 +0100

    Day 11 - Part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 advent_of_code::solution!(11);
 
+const EXPANSION_RATIO: u128 = 1000000;
+
 pub fn part_one(input: &str) -> Option<usize> {
     let mut sum = 0;
     let mut char_mat = build_matrix(input);
@@ -8,7 +10,7 @@ pub fn part_one(input: &str) -> Option<usize> {
     char_mat = expand_vertically(char_mat);
     let galaxies_coords = get_galaxies_coords(&char_mat);
     let distances = get_galaxies_distances(galaxies_coords);
-    for (pair, d) in distances {
+    for (_, d) in distances {
         sum += d;
     }
     Some(sum)
@@ -87,8 +89,82 @@ fn get_galaxies_distances(galaxies_coords: Vec<(usize, usize)>) -> HashMap<((usi
     distances
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u128> {
+    let mut sum = 0;
+    let char_mat = build_matrix(input);
+    let rows_weights = get_rows_weights(&char_mat);
+    let columns_weights = get_columns_weights(&char_mat);
+    let galaxies_coords = get_galaxies_coords(&char_mat);
+    let distances = get_galaxies_distances_with_weights(galaxies_coords, rows_weights, columns_weights);
+    for (_, d) in distances {
+        sum += d;
+    }
+    Some(sum)
+}
+
+fn get_rows_weights(char_mat: &Vec<Vec<char>>) -> Vec<u128> {
+    let mut weights = vec![];
+    for row in char_mat.iter() {
+        if row.iter().filter(|c| **c == '#').collect::<Vec<&char>>().len() == 0 {
+            weights.push(EXPANSION_RATIO);
+        } else {
+            weights.push(1)
+        }
+    }
+    weights
+}
+
+fn get_columns_weights(char_mat: &Vec<Vec<char>>) -> Vec<u128> {
+    let mut empty_columns = vec![true; char_mat[0].len()];
+    for row in char_mat.iter() {
+        for (j, c) in row.iter().enumerate() {
+            if *c == '#' {
+                empty_columns[j] = false;
+            }
+        }
+    }
+
+    let weights = empty_columns
+        .iter()
+        .map(|&x| if x { EXPANSION_RATIO } else { 1 })
+        .collect::<Vec<u128>>();
+    weights
+}
+
+fn get_galaxies_distances_with_weights(
+    galaxies_coords: Vec<(usize, usize)>,
+    rows_weights: Vec<u128>,
+    columns_weights: Vec<u128>,
+) -> HashMap<((usize, usize), (usize, usize)), u128> {
+    let mut distances: HashMap<((usize, usize), (usize, usize)), u128> = HashMap::new();
+    for i in 0..galaxies_coords.len() {
+        for j in (i + 1)..galaxies_coords.len() {
+            let galaxy_coord_1 = galaxies_coords[i];
+            let galaxy_coord_2 = galaxies_coords[j];
+            let row_distance = get_distance_with_weights(galaxy_coord_1.0, galaxy_coord_2.0, &rows_weights);
+            let column_distance = get_distance_with_weights(galaxy_coord_1.1, galaxy_coord_2.1, &columns_weights);
+            let d = row_distance + column_distance;
+            distances.insert((galaxy_coord_1, galaxy_coord_2), d);
+        }
+    }
+    distances
+}
+
+fn get_distance_with_weights(index_1: usize, index_2: usize, weights: &Vec<u128>) -> u128 {
+    let mut d: u128 = 0;
+    let start: usize;
+    let end: usize;
+    if index_1 < index_2 {
+        start = index_1;
+        end = index_2;
+    } else {
+        start = index_2;
+        end = index_1;
+    }
+    for i in start..end {
+        d += weights[i];
+    }
+    d
 }
 
 #[cfg(test)]
@@ -104,6 +180,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(82000210));
     }
 }
