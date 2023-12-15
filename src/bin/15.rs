748831627cbcd17d04ce81772043df50c1b6f750
Dantos7 <daniel.tosone@gmail.com>
2023-12-15 10:12:16 +0100

    Day 15 - Part 2

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 advent_of_code::solution!(15);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -28,7 +29,71 @@ fn hash_algorithm(input: &str) -> u32 {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let line = input.split("\n").nth(0).unwrap();
+    let steps = line.split(",");
+    let mut box_to_lenses: HashMap<u8, Vec<(&str, u32)>> = HashMap::new();
+    for step in steps {
+        if step.contains("-") {
+            removal_op(step, &mut box_to_lenses)
+        } else {
+            addition_op(step, &mut box_to_lenses)
+        }
+    }
+    let sum = compute_sum(box_to_lenses);
+    Some(sum)
+}
+
+fn removal_op<'a>(step: &'a str, box_to_lenses: &mut HashMap<u8, Vec<(&'a str, u32)>>) {
+    let label = step.split("-").nth(0).unwrap();
+    let box_index = hash_algorithm(label) as u8;
+    let lenses = box_to_lenses.get(&box_index);
+    if lenses.is_none() {
+        box_to_lenses.insert(box_index, vec![]);
+    } else {
+        let mut lenses_vec = lenses.unwrap().clone();
+        for (i, (l, _)) in lenses_vec.iter().enumerate() {
+            if label == *l {
+                lenses_vec.remove(i);
+                box_to_lenses.insert(box_index, lenses_vec);
+                break;
+            }
+        }
+    }
+}
+
+fn addition_op<'a>(step: &'a str, box_to_lenses: &mut HashMap<u8, Vec<(&'a str, u32)>>) {
+    let mut step_split = step.split("=");
+    let label = step_split.nth(0).unwrap();
+    let focal_length = step_split.nth(0).unwrap().parse::<u32>().unwrap();
+    let box_index = hash_algorithm(label) as u8;
+    let lenses = box_to_lenses.get(&box_index);
+    if lenses.is_none() {
+        box_to_lenses.insert(box_index, vec![(label, focal_length)]);
+    } else {
+        let mut lenses_vec = lenses.unwrap().clone();
+        for (i, (l, _)) in lenses_vec.iter().enumerate() {
+            if label == *l {
+                lenses_vec[i] = (*l, focal_length);
+                box_to_lenses.insert(box_index, lenses_vec);
+                return;
+            }
+        }
+        // if the cycle didn't return, then the lens with label is not in the box already -> push it to the back
+        lenses_vec.push((label, focal_length));
+        box_to_lenses.insert(box_index, lenses_vec);
+    }
+}
+
+fn compute_sum(box_to_lenses: HashMap<u8, Vec<(&str, u32)>>) -> u32 {
+    let mut sum = 0;
+    for (k, v) in box_to_lenses {
+        let box_number: u32 = k as u32 + 1;
+        for (i, (_, f)) in v.iter().enumerate() {
+            let lens_position_number = i as u32 + 1;
+            sum += box_number * lens_position_number * f;
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -44,6 +109,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(145));
     }
 }
